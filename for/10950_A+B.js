411bb1a56bc474d59765da2c563d6550703b357b
origin <2206017@naver.com>
2022-02-01 18:18:41 +0900

    10950_A+B 문제 해결

--- for/10950_A+B.js
+++ for/10950_A+B.js
@@ -1,1 +1,19 @@
- 
+var readline = require('readline');
+var r = readline.createInterface({ 
+    input:process.stdin, 
+    output:process.stdout 
+});
+
+let count = 0;
+let result = [];
+r.on('line', (input) => {
+    result.push(input);
+}).on('close', function(){
+    count = parseInt(result.shift());
+    for(let i=0; i<result.length; i++){
+        let [A, B] = result[i].split(" ").map(Number);
+        if((A > 0 && A < 10) && (B > 0 && B < 10)){
+            console.log(A + B);
+        }
+    }
+});
