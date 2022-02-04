63a50d650ca92d59f2bdc96f7634a30c0e58d0ab
MS <2206017@naver.com>
2022-02-04 15:42:06 +0900

    BOJ 10952

--- while/10952_addAB.js
+++ while/10952_addAB.js
@@ -5,8 +5,15 @@ const r = readline.createInterface({
 });
 
 let answer = "";
+let tmp = [];
 r.on('line', (input) => {
-
+    let a = parseInt(input.split(" ")[0]);
+    let b = parseInt(input.split(" ")[1]);
+    if(a === 0 && b === 0){
+        r.close();
+    }else{
+        answer += (a + b) + '\n';
+    }
 }).on('close', function(){
     console.log(answer);
     process.exit();
