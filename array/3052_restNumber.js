b0ae88d89dd99e3c8ca4573ea0ac1e204a4c2775
origin <2206017@naver.com>
2022-02-05 22:05:48 +0900

    BOJ 3052 중간저장

---
+++ array/3052_restNumber.js
@@ -0,0 +1,19 @@
+//let input = require("fs").readFileSync('/dev/stdin').toString();
+let input = `39
+40
+41
+42
+43
+44
+82
+83
+84
+85`;
+let firstArr = input.split("\n").map(Number);
+let answer = ''; secondArr = [];
+for(let i=0; i<firstArr.length; i++){
+    secondArr.push(tmp[i] % 42);
+    for(let j=0; j<secondArr.length; j++){
+        
+    }
+}
