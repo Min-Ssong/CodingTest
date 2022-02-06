229427021fc98dfb73aba75ee7570729b1aaec75
MS <2206017@naver.com>
2022-02-06 16:47:58 +0900

    BOJ 3052 clear

--- array/3052_restNumber.js
+++ array/3052_restNumber.js
@@ -1,19 +1,9 @@
-//let input = require("fs").readFileSync('/dev/stdin').toString();
-let input = `39
-40
-41
-42
-43
-44
-82
-83
-84
-85`;
-let firstArr = input.split("\n").map(Number);
-let answer = ''; secondArr = [];
-for(let i=0; i<firstArr.length; i++){
-    secondArr.push(tmp[i] % 42);
-    for(let j=0; j<secondArr.length; j++){
-        
+let input = require("fs").readFileSync('/dev/stdin').toString().trim().split("\n").map(Number);
+let secondArr = [];
+input.forEach(x =>{
+    let num = x % 42;
+    if(secondArr.indexOf(num) === -1){
+        secondArr.push(num);
     }
-}
+});
+console.log(secondArr.length);
