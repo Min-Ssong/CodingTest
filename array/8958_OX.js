c03a2b6168eeb09636b80b2437504eaf71faab38
origin <2206017@naver.com>
2022-02-09 06:08:55 +0900

    BOJ 8958 clear

--- array/8958_OX.js
+++ array/8958_OX.js
@@ -1,16 +1,21 @@
-//let input = require("fs").readFileSync('/dev/stdin').toString().trim();
-let input = `5
-OOXXOXXOOO
-OOXXOOXXOO
-OXOXOXOXOXOXOX
-OOOOOOOOOO
-OOOOXOOOOXOOOOX`;
+let input = require("fs").readFileSync('/dev/stdin').toString().trim();
 let originArr = input.split('\n');
+let count = 0; result = 0;
 originArr.shift();
 for(let i=0; i<originArr.length; i++){
     let tmp = originArr[i];
-    
+    for(let j=0; j<tmp.length; j++){
+        if(tmp[j] == 'O'){
+            if(count == 0)
+                count = 1;
+            else
+                count += 1;
+        }else
+            count = 0;
+        result += count;
+    }
+    console.log(result);
+
+    count = 0;
+    result = 0;
 }
-// 1. originArr에서 한 줄씩 임시 배열에 저장
-// 2. O -> 1 / X -> 0으로 치환
-// 3. O면 앞자리 + 1로 해서 저장 / O인데 0번째 자리면 1
