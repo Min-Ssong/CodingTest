99910ca055c6cbd8d3127df31d7fb778df797046
origin <2206017@naver.com>
2022-02-14 06:28:17 +0900

    BOJ 1065 중간 저장_1

--- function/1065_oneNum.js
+++ function/1065_oneNum.js
@@ -1,2 +1,11 @@
 //let input = require("fs").readFileSync('/dev/stdin').toString().trim();
 let input = `110`;
+function oneNum(num){
+    for(let i=1; i<=num; i++){
+        let strNum = String(i).split('').map(Number);
+        strNum.forEach(x => {
+            
+        }); 
+    }
+}
+oneNum(input);
