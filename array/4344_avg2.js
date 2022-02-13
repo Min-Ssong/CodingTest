6f2cc71f3e5c7411773f4f614d0d237dd785f9ab
MS <2206017@naver.com>
2022-02-13 14:03:46 +0900

    BOJ 4344 clear_2

--- array/4344_avg2.js
+++ array/4344_avg2.js
@@ -1,10 +1,4 @@
-//let input = require("fs").readFileSync('/dev/stdin').toString().trim();
-let input = `5
-5 50 50 70 80 100
-7 100 95 90 80 70 60 50
-3 70 90 80
-3 70 90 81
-9 100 99 98 97 96 95 94 93 91`;
+let input = require("fs").readFileSync('/dev/stdin').toString().trim();
 let originArr = [];
 let sumArr = [];
 let sum = 0;
@@ -34,6 +28,3 @@ for(let i=0; i<originArr.length; i++){
     sum = 0;
 }
 console.log(answer);
-// 1. 점수로만 이뤄진 배열 만들기
-// 2. 평균 구하기
-// 3. 점수 중에서 평균보다 큰 점수의 비율 구하기
