1edafbd7c97e6ba3154ce1be859d212b8e90b2dd
MS <2206017@naver.com>
2022-04-28 14:33:33 +0900

    BOJ 2908 update

--- string/2908_sangSu.js
+++ string/2908_sangSu.js
@@ -1,16 +1,15 @@
 const input = require("fs").readFileSync('/dev/stdin').toString().trim().split(' ');
 //const input = `734 893`.split(' ');
-const numbers = [input[0], input[1]];
 
 const chNum1 
-    = numbers[0].split('')[2] 
-    + numbers[0].split('')[1] 
-    + numbers[0].split('')[0];
+    = input[0].split('')[2] 
+    + input[0].split('')[1] 
+    + input[0].split('')[0];
 
 const chNum2 
-    = numbers[1].split('')[2] 
-    + numbers[1].split('')[1] 
-    + numbers[1].split('')[0];
+    = input[1].split('')[2] 
+    + input[1].split('')[1] 
+    + input[1].split('')[0];
 
 let result = chNum1 > chNum2 ? chNum1 : chNum2;
 console.log(result);
