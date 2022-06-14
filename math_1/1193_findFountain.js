9f35c5927ff113524a2df922590d82726599c2ad
MS <2206017@naver.com>
2022-06-14 09:27:01 +0900

    BOJ 1193 clear

---
+++ math_1/1193_findFountain.js
@@ -0,0 +1,13 @@
+let input = Number(require('fs').readFileSync('/dev/stdin').toString().trim());
+let counter = 0;
+
+while (input > 0){
+    counter++;
+    input = input - counter;
+}
+
+if(counter % 2 === 0){
+    console.log(`${counter + input}/${1 + -input}`);
+}else{
+    console.log(`${1 + -input}/${counter + input}`);
+}
