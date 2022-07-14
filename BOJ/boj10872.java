21ac434f11f633886c2d428feec2b8308034271a
MS <2206017@naver.com>
2022-07-14 17:10:45 +0900

    create boj10872

---
+++ BOJ/boj10872.java
@@ -0,0 +1,16 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class boj10872 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = 1;
+        for(int i = n; i > 0; i--){
+            result *= i;
+        }
+        System.out.print(result);
+        sc.close();
+    }
+}
