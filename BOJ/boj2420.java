001d6f39bd5138212b68ec7d56000751ead7f671
MS <2206017@naver.com>
2022-07-14 17:01:19 +0900

    create boj2420

---
+++ BOJ/boj2420.java
@@ -0,0 +1,15 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class boj2420 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] originStr = sc.nextLine().trim().split(" ");
+        long N = Long.parseLong(originStr[0]);
+        long M = Long.parseLong(originStr[1]);
+
+        System.out.print(Math.abs(N-M));
+        sc.close();
+    }
+}
