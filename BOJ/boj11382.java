f8b80e9c2f7e5b3bf1913d1b651fff0d472b6ff1
MS <2206017@naver.com>
2022-07-14 16:51:02 +0900

    create boj11382

---
+++ BOJ/boj11382.java
@@ -0,0 +1,18 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class boj11382 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] str = in.nextLine().trim().split(" ");
+        long result = 0;
+
+        for(int i = 0; i < str.length; i++) {
+            result += Long.parseLong(str[i]);
+        }
+
+        System.out.print(result);
+        in.close();
+    }
+}
