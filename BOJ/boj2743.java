f416ede8475fa81fe6e561fdd3891edf8ef93518
MS <2206017@naver.com>
2022-07-25 14:55:04 +0900

    create boj2743

---
+++ BOJ/boj2743.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class boj2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("%d", sc.next().length());
+    }
+}
