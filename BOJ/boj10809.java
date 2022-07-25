a7bbb6f3dcd43584f51910ab20e366d811a7d7d8
MS <2206017@naver.com>
2022-07-25 17:04:09 +0900

    create boj10809

---
+++ BOJ/boj10809.java
@@ -0,0 +1,16 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class boj10809 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+
+        for( int j = 97; j < 123; j++ ){
+            System.out.printf("%d ", str.indexOf((char)j));
+        }
+        
+        sc.close();
+    }
+}
