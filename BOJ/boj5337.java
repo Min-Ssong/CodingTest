0aaf26571c778602d252e58c08a294f6a3ae3a85
MS <2206017@naver.com>
2022-07-26 17:34:56 +0900

    create boj5337

---
+++ BOJ/boj5337.java
@@ -0,0 +1,10 @@
+package BOJ;
+
+public class boj5337 {
+    public static void main(String[] args) {
+        String str = ".  .   .\n"+
+        "|  | _ | _. _ ._ _  _\n"+
+        "|/\\|(/.|(_.(_)[ | )(/.";
+        System.out.println(str);
+    }
+}
