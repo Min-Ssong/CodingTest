3f52ee7da74544ac271fc0c1a3b6f592a07331a4
MS <2206017@naver.com>
2022-07-27 14:58:05 +0900

    create boj17256

---
+++ BOJ/javaAlgo/boj17256.java
@@ -0,0 +1,18 @@
+package BOJ.javaAlgo;
+
+import java.util.Scanner;
+
+public class boj17256 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        String c = sc.nextLine();
+        int b_0 = Integer.parseInt(c.trim().split(" ")[0]) - Integer.parseInt(a.trim().split(" ")[2]);
+        int b_1 = Integer.parseInt(c.trim().split(" ")[1]) / Integer.parseInt(a.trim().split(" ")[1]);
+        int b_2 = Integer.parseInt(c.trim().split(" ")[2]) - Integer.parseInt(a.trim().split(" ")[0]);
+        System.out.printf("%d %d %d", b_0, b_1, b_2);
+
+        sc.close();
+    }    
+}
