ae0c08700940b30f68819ee228c6f444be30e280
MS <2206017@naver.com>
2022-07-28 12:50:50 +0900

    create boj3046

---
+++ BOJ/javaAlgo/boj3046.java
@@ -0,0 +1,11 @@
+package BOJ.javaAlgo;
+
+import java.util.Scanner;
+
+public class boj3046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(-sc.nextInt() + (sc.nextInt() * 2));
+        sc.close();
+    }
+}
