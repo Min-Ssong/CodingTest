b01331067b03db00866a05aa0e8901552ed52407
MS <2206017@naver.com>
2022-08-01 14:59:19 +0900

    create boj6763

---
+++ BOJ/javaAlgo/boj6763.java
@@ -0,0 +1,24 @@
+package BOJ.javaAlgo;
+
+import java.util.Scanner;
+
+public class boj6763 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int limit = sc.nextInt();
+        int mySpeed = sc.nextInt();
+        int over = mySpeed - limit;
+        System.out.println(over);
+        if ( over <= 0 ) {
+            System.out.println("Congratulations, you are within the speed limit!");
+        } else if ( 1 <= over && over <= 20 ) {
+            System.out.println("You are speeding and your fine is $100.");
+        } else if ( 21 <= over && over <= 30 ) {
+            System.out.println("You are speeding and your fine is $270.");
+        } else {
+            System.out.println("You are speeding and your fine is $500.");
+        }
+        System.out.println();
+        sc.close();
+    }
+}
