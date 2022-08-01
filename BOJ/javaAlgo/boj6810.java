7b694be2c5eab5b6e6b4d305008f0f92e2d35a82
MS <2206017@naver.com>
2022-08-01 15:43:12 +0900

    create boj6810

---
+++ BOJ/javaAlgo/boj6810.java
@@ -0,0 +1,12 @@
+package BOJ.javaAlgo;
+
+import java.util.Scanner;
+
+public class boj6810 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int result = sc.nextInt() + (sc.nextInt() * 3) + sc.nextInt();
+        System.out.printf("The 1-3-sum is %d", result + 91);
+        sc.close();
+    }    
+}
