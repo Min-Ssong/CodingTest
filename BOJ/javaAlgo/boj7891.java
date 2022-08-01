25cb2ab5f3f59735b44720b67eadb359da6beac8
MS <2206017@naver.com>
2022-08-01 16:31:09 +0900

    create boj7891

---
+++ BOJ/javaAlgo/boj7891.java
@@ -0,0 +1,22 @@
+package BOJ.javaAlgo;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class boj7891 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        String[] result = new String[len];
+        for( int i = 0; i < len ; i++ ) {
+            BigInteger bi_1 = new BigInteger(sc.next());
+            result[i] = String.valueOf(bi_1.add(new BigInteger(sc.next())));
+        }
+
+        for (String aString : result) {
+            System.out.println(aString);
+        }
+        
+        sc.close();
+    }
+}
