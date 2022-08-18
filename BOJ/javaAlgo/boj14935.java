a8c4038e4ca4fb231d89ff917f178db49706fbb6
MS <2206017@naver.com>
2022-08-18 16:00:48 +0900

    create boj14935

---
+++ BOJ/javaAlgo/boj14935.java
@@ -0,0 +1,12 @@
+package BOJ.javaAlgo;
+
+import java.util.Scanner;
+
+public class boj14935 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String result = sc.nextLine();
+        System.out.print("FA");
+        sc.close();
+    }
+}
