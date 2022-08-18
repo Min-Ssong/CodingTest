d2cea4fc8a1def5b82f41aab1f9821852dcec4d0
MS <2206017@naver.com>
2022-08-18 14:43:40 +0900

    create boj14489

---
+++ BOJ/javaAlgo/boj14489.java
@@ -0,0 +1,16 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj14489 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String money = br.readLine();
+        int a = Integer.parseInt(money.split(" ")[0]);
+        int b = Integer.parseInt(money.split(" ")[1]);
+        int chickens = Integer.parseInt(br.readLine()) * 2;
+        System.out.print( ( a + b >= chickens ) ? (a + b) - chickens : a + b );
+
+        br.close();
+    }
+}
