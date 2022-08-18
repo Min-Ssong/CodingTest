ab455d73e9babf1ea68ac1d308848de67d749400
MS <2206017@naver.com>
2022-08-18 14:12:21 +0900

    create boj14470

---
+++ BOJ/javaAlgo/boj14470.java
@@ -0,0 +1,21 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj14470 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+        int D = Integer.parseInt(br.readLine());
+        int E = Integer.parseInt(br.readLine());
+        if ( A < 0 ) {
+            System.out.print(((Math.abs(A) * C) + D) + ( B * E ));
+        } else {
+            System.out.print((B-A)*E);
+
+        }
+        br.close();
+    }
+}
