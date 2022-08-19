a3b407f5f1327bc285dbf37dbe8b07004576da7c
MS <2206017@naver.com>
2022-08-19 16:20:04 +0900

    create boj15051

--- BOJ/javaAlgo/boj15051.java
+++ BOJ/javaAlgo/boj15051.java
@@ -8,21 +8,14 @@ public class boj15051 {
         int[] humans = {Integer.parseInt(br.readLine()),
             Integer.parseInt(br.readLine()),
             Integer.parseInt(br.readLine())};
-        int num = 0;
-        if ( humans[0] > humans[1] ) {
-            num = ( humans[0] > humans[2] ) ? 0 : 2;
-        } else if ( humans[0] < humans[1] ){
-            num = ( humans[1] > humans[2] ) ? 1 : 2;
-        } else if ( humans[0] == humans[1] || humans[1] == humans[2]) {
-            num = 1;
-        } 
-
-        if ( num == 0 ) {
-            System.out.print(humans[1] * 2 + humans[2] * 4);
-        } else if ( num == 1 ) {
-            System.out.print(humans[0] * 2 + humans[2] * 2);
-        } else if ( num == 2 ) {
-            System.out.print(humans[0] * 4 + humans[1] * 2);
+        int[] result = { ( humans[1] * 2 ) + ( humans[2] * 4 ),
+            ( humans[0] * 2 ) + ( humans[2] * 2 ),
+            ( humans[0] * 4 ) + ( humans[1] * 2 )};
+        
+        if ( result[0] < result[1] ) {
+            System.out.println(Math.min(result[0], result[2]));
+        } else {
+            System.out.println(Math.min(result[1], result[2]));
         }
 
         br.close();
