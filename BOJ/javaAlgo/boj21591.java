55dd1e401f1880a1049e8442acdce04a78b1ff9b
origin <2206017@naver.com>
2022-08-22 09:32:55 +0900

    update boj21591

--- BOJ/javaAlgo/boj21591.java
+++ BOJ/javaAlgo/boj21591.java
@@ -9,7 +9,7 @@ public class boj21591 {
         int width = Integer.parseInt(input.split(" ")[0]) - Integer.parseInt(input.split(" ")[2]);
         int height = Integer.parseInt(input.split(" ")[1]) - Integer.parseInt(input.split(" ")[3]);
 
-        System.out.print( ( width >= 1 && height >= 1 ) ? 1 : 0 );
+        System.out.print( ( width > 1 && height > 1 ) ? 1 : 0 );
         br.close();
     }
 }
