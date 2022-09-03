8a0c88b9b133444ef582f9700a6285f1822e292e
origin <2206017@naver.com>
2022-09-03 20:48:01 +0900

    create boj4470

---
+++ BOJ/javaAlgo/boj4470.java
@@ -0,0 +1,17 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj4470 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        String result = "";
+        for ( int i = 1; i <= num; i++ ) {
+            String temp = br.readLine();
+            result += i + ". " + temp + "\n";
+        }
+        System.out.print(result);
+        br.close();
+    }
+}
