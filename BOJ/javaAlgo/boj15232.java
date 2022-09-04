889fb716cd9468d726e9fe0b59e9be500f6af995
origin <2206017@naver.com>
2022-09-04 16:09:22 +0900

    create boj15232

---
+++ BOJ/javaAlgo/boj15232.java
@@ -0,0 +1,19 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj15232 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int line = Integer.parseInt(br.readLine());
+        int num = Integer.parseInt(br.readLine());
+        for ( int i = 0; i < line; i++ ) {
+            if ( i == line - 1) {
+                System.out.print("*".repeat(num));
+                break;
+            }
+            System.out.print("*".repeat(num) + "\n");
+        }
+        br.close();
+    }
+}
