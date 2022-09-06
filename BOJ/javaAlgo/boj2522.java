c7dbc53d4df42270f4f2edc9595d615acf68b6e8
MS <2206017@naver.com>
2022-09-06 17:51:41 +0900

    create boj2522

---
+++ BOJ/javaAlgo/boj2522.java
@@ -0,0 +1,23 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj2522 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for ( int i = N-1; i >= 0; i-- ) {
+            System.out.printf("%s%s\n",
+                " ".repeat(i),
+                "*".repeat(N-i)
+            );
+        }
+        for ( int i = 1; i < N; i++ ) {
+            System.out.printf("%s%s\n",
+                " ".repeat(i),
+                "*".repeat(N-i)
+            );
+        }
+        br.close();
+    }
+}
