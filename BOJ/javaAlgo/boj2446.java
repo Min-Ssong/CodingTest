ab99c1cf5c084b2edfa060a852b50724181e1ff2
MS <2206017@naver.com>
2022-09-06 17:46:38 +0900

    create boj2446

---
+++ BOJ/javaAlgo/boj2446.java
@@ -0,0 +1,24 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj2446 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int num = N * 2 - 1;
+        for ( int i = 0; i < N; i++ ) {
+            System.out.printf("%s%s\n",
+                " ".repeat(i),
+                "*".repeat(num - (i*2))
+            );
+        }
+        for ( int i = N - 2; i >= 0; i-- ) {
+            System.out.printf("%s%s\n",
+                " ".repeat(i),
+                "*".repeat(num - (i*2))
+            );
+        }
+        br.close();
+    }
+}
