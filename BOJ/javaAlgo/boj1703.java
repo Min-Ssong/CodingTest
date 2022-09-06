313096563074e52ab58e3469300a0b903f46a014
MS <2206017@naver.com>
2022-09-06 15:44:33 +0900

    create boj1703

---
+++ BOJ/javaAlgo/boj1703.java
@@ -0,0 +1,33 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class boj1703 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String result = "";
+        while ( true ) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+                
+            int tree = 1;
+            
+            if(N == 0) {
+                System.out.print(result);
+                break;
+            }
+            
+            for(int i = 0; i < 2 * N; i++) {
+                int M = Integer.parseInt(st.nextToken());
+                if(i % 2 == 0) {
+                    tree *= M;
+                }else {
+                    tree -= M;
+                }
+            }
+            result += tree + "\n";
+        }
+        br.close();
+    }
+}
