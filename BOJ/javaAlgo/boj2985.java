c6b98e218b94e64f3813bbfae1229b4b0aab7d89
MS <2206017@naver.com>
2022-09-07 16:48:05 +0900

    create boj2985

---
+++ BOJ/javaAlgo/boj2985.java
@@ -0,0 +1,33 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj2985 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String allInput = br.readLine();
+        int A = Integer.parseInt(allInput.split(" ")[0]);
+        int B = Integer.parseInt(allInput.split(" ")[1]);
+        int C = Integer.parseInt(allInput.split(" ")[2]);
+
+        if ( A + B == C ) {
+            System.out.printf("%d+%d=%d", A, B, C);
+        } else if ( A - B == C ) {
+            System.out.printf("%d-%d=%d", A, B, C);
+        } else if ( A * B == C ) {
+            System.out.printf("%d*%d=%d", A, B, C);
+        } else if ( A / B == C ) {
+            System.out.printf("%d/%d=%d", A, B, C);
+        } else if ( A == B + C ) {
+            System.out.printf("%d=%d+%d", A, B, C);
+        } else if ( A == B - C ) {
+            System.out.printf("%d=%d-%d", A, B, C);
+        } else if ( A == B * C ) {
+            System.out.printf("%d=%d*%d", A, B, C);
+        } else if ( A == B / C ) {
+            System.out.printf("%d=%d/%d", A, B, C);
+        }
+        
+        br.close();
+    }
+}
