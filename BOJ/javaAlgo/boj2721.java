1a68fe12fe956b30584f9fb4f030b0b224ef6d34
MS <2206017@naver.com>
2022-09-07 11:42:46 +0900

    create boj2721

---
+++ BOJ/javaAlgo/boj2721.java
@@ -0,0 +1,31 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class boj2721 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boj2721 boj = new boj2721();
+        int T = Integer.parseInt(br.readLine());
+        String result = "";
+        for ( int i = 0; i < T; i++ ) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int temp = 0;
+            for ( int k = 1; k <= n; k++ ) {
+                temp += boj.Tn(k+1) * k;
+            }
+            result += temp + "\n";
+        }
+        System.out.print(result);
+        br.close();
+    }
+    int Tn (int n) {
+        int num = 0;
+        for ( int i = 1; i <= n; i++ ) {
+            num += i;
+        }
+        return num;
+    }
+}
