0894213791e26b4f1a8eecb78d16513957e5fe30
MS <2206017@naver.com>
2022-09-07 13:55:20 +0900

    create boj2857

---
+++ BOJ/javaAlgo/boj2857.java
@@ -0,0 +1,18 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj2857 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String result = "";
+        for ( int i = 1; i <= 5; i++ ) {
+            String name = br.readLine();
+            if( name.indexOf("FBI") != -1 ) {
+                result += i + " ";
+            }
+        }
+        System.out.print((result.equals("")) ? "HE GOT AWAY!" : result);
+        br.close();
+    }
+}
