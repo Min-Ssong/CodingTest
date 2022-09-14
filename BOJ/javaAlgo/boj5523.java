c98b1ddd681abafdeffbb8e724ad67256274e37a
MS <2206017@naver.com>
2022-09-14 17:33:04 +0900

    create boj5523

---
+++ BOJ/javaAlgo/boj5523.java
@@ -0,0 +1,24 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj5523 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int round = Integer.parseInt(br.readLine());
+        int winnerA = 0;
+        int winnerB = 0;
+        for ( int i = 0; i < round; i++ ) {
+            String AB = br.readLine();
+            int A = Integer.parseInt(AB.split(" ")[0]);
+            int B = Integer.parseInt(AB.split(" ")[1]);
+            if ( A > B ) {
+                winnerA += 1;
+            } else if ( A < B ) {
+                winnerB += 1;
+            }
+        }
+        System.out.println(winnerA + " " + winnerB);
+        br.close();
+    }
+}
