42ef6fc4c3975ed97d46b417c306e488f16c94a4
MS <2206017@naver.com>
2022-09-19 15:34:44 +0900

    create pr01 update gitignore

---
+++ programmers/pr01.java
@@ -0,0 +1,15 @@
+package programmers;
+
+class Solution {
+    public String solution(int num) {
+        return (num % 2 != 0) ? "Odd" : "Even";
+    }
+}
+
+public class pr01 {
+    public static void main(String[] args) {
+        int num = 4;
+		Solution sol = new Solution();
+        System.out.print(sol.solution(num));
+	}
+}
