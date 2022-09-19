25f9a5c43ea05c47bd1d31850176c5a150684515
MS <2206017@naver.com>
2022-09-19 17:27:39 +0900

    create pr06

---
+++ programmers/pr06.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class pr06 {
+    public static void main(String[] args) {
+        Long n = 36765L;
+        String[] temp = String.valueOf(n).split("");
+        int[] answer = new int[temp.length];
+        for ( int i = temp.length - 1; i >= 0; i-- ) {
+            answer[temp.length - 1 - i] = Integer.parseInt(temp[i]);
+        }
+        System.out.println(answer);
+    }
+}
