2eb94a95912664fae226f25abcb3edce8728819a
MS <2206017@naver.com>
2022-09-19 16:07:20 +0900

    create pr04 update pr01, pr02

--- programmers/pr02.java
+++ programmers/pr02.java
@@ -1,20 +1,13 @@
 package programmers;
 
-class Solution {
-    public int solution(int n) {
-        int answer = 0;
+public class pr02 {
+    public static void main(String[] args) {
+        int n = 1;
+		int answer = 0;
         while ( n >= 10 ) {
             answer += n % 10;
             n = n / 10;
         }
-        return answer + n;
-    }
-}
-
-public class pr02 {
-    public static void main(String[] args) {
-        int num = 1;
-		Solution sol = new Solution();
-        System.out.print(sol.solution(num));
+        System.out.print(answer + n);
 	}
 }
