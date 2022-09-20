2165f8649d9db0ebc9eef5e0d84ce3aa5a393e2c
MS <2206017@naver.com>
2022-09-20 16:53:46 +0900

    create pr13

---
+++ programmers/pr13.java
@@ -0,0 +1,26 @@
+package programmers;
+
+public class pr13 {
+    public static void main(String[] args) {
+        int num = 626331;
+        long numlong = num;
+        int answer = 1;
+        if ( numlong == 1 ) {
+            System.out.println(0);
+        } else {
+            while ( true ) {
+                if ( answer == 500 ) {
+                    System.out.printf("%d %d", numlong, answer);
+                    break;
+                }
+
+                if ( numlong == 1 ) {
+                    System.out.println(answer);
+                } else {
+                    numlong = ( numlong % 2 == 0 ) ? numlong / 2 : numlong * 3 + 1;
+                    answer++;
+                }
+            }
+        }
+    }
+}
