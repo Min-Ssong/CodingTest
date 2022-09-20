6fb896be161205bf24cc54940fd58f801ef99153
MS <2206017@naver.com>
2022-09-20 17:14:24 +0900

    create pr16

---
+++ programmers/pr15.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class pr15 {
+    public static void main(String[] args) {
+        String[] seoul = { "Jane", "Kim" };
+        String answer = "";
+
+        for ( int i = 0; i < seoul.length; i++ ) {
+            if ( seoul[i].equals("Kim") ) {
+                answer = String.format("김서방은 %d에 있다", i);
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
