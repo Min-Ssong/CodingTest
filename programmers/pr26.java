0b21672de030e3e4810aa45cd069616a45a8e245
MS <2206017@naver.com>
2022-09-22 15:51:37 +0900

    create pr26

---
+++ programmers/pr26.java
@@ -0,0 +1,20 @@
+package programmers;
+
+public class pr26 {
+    public static void main(String[] args) {
+        boolean answer = true;
+        String s = "1e10";
+
+        if ( s.length() != 4 && s.length() != 6 ) {
+            answer = false;
+        }
+
+        for (char ch : s.toCharArray()) {
+            int num = (int)ch;
+            if ( num < 48 || num > 57 ) {
+                answer = false;
+            }
+        }
+        System.out.println(answer);
+    }
+}
