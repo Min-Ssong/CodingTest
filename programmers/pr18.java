b487861d7c1e545acf83d68744927caa2438e840
MS <2206017@naver.com>
2022-09-22 09:29:46 +0900

    create pr18 - 프로그래머스 "제일 작은 수 제거하기" 코드는 작성했으나 런타임에러로 문제를 풀지못함...

---
+++ programmers/pr18.java
@@ -0,0 +1,27 @@
+package programmers;
+
+public class pr18 {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,1};
+        int[] answer =  new int[( arr.length != 1 ) ? arr.length - 1 : 1];
+        int min = Integer.MAX_VALUE;
+        for ( int i = 0; i < arr.length; i++ ) {
+            if ( min != arr[i]) {
+                min = ( min > arr[i]) ? arr[i] : min;
+            }
+        }
+        if ( arr.length == 1 ) {
+            answer[0] = -1;
+        } else {
+            for ( int i = 0; i < arr.length; i++ ) {
+                if ( arr[i] != min ) {
+                    answer[i] = arr[i];
+                }
+            }
+        }
+
+        for (int i : answer) {
+            System.out.print(i + " ");
+        }
+    }
+}
