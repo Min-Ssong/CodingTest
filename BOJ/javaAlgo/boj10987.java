529c924d0933120bb9d68808850ba2d318b2b930
MS <2206017@naver.com>
2022-09-23 15:48:13 +0900

    create boj10987

---
+++ BOJ/javaAlgo/boj10987.java
@@ -0,0 +1,20 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj10987 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        char[] ch = word.toCharArray();
+        int count = 0;
+        for ( int i = 0; i < ch.length; i++ ) {
+            int num = (int)ch[i];
+            if ( num == 97 || num == 101 || num == 105 || num == 111 || num == 117 ) {
+                count += 1;
+            }
+        }
+        System.out.print(count);
+        br.close();
+    }
+}
