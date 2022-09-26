5ae0c3aa592f8918c0da966cf0efef39523d1809
MS <2206017@naver.com>
2022-09-26 14:08:14 +0900

    create boj24263

---
+++ BOJ/javaAlgo/boj24263.java
@@ -0,0 +1,11 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj24263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.printf("%s\n1", br.readLine());
+        br.close();
+    }
+}
