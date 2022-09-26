4dd40da0eb7b4ecc863b52c917073e593f7301da
MS <2206017@naver.com>
2022-09-26 15:26:29 +0900

    create boj25642

---
+++ BOJ/javaAlgo/boj25642.java
@@ -0,0 +1,29 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class boj25642 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int ytFinger = Integer.parseInt(st.nextToken());
+        int yjFinger = Integer.parseInt(st.nextToken());
+        int count = 1;
+        while ( true ) {
+            if ( ytFinger > 4 || yjFinger > 4 ) {
+                System.out.print((ytFinger > yjFinger) ? "yj" : "yt");
+                break;
+            }
+            
+            if ( count % 2 == 0 ) {
+                ytFinger += yjFinger;
+            } else {
+                yjFinger += ytFinger;
+            }
+
+            count += 1;
+        }
+        br.close();
+    }
+}
