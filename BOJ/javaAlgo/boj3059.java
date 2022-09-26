2e73de0f0b667314b4845b4ede2fa5839346d986
MS <2206017@naver.com>
2022-09-26 15:58:51 +0900

    create boj3059

---
+++ BOJ/javaAlgo/boj3059.java
@@ -0,0 +1,28 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj3059 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for ( int i = 0; i < T; i++ ) {
+            String s = br.readLine();
+            int[] alphabet = new int[26];
+            for ( int j = 0; j < s.length(); j++ ) {
+                alphabet[(int)s.charAt(j) - 65] += 1;
+            }
+
+            int num = 0;
+            for ( int k = 0; k < alphabet.length; k++ ) {
+                if ( alphabet[k] == 0 ) {
+                    num += k + 65;
+                }
+            }
+            sb.append(num).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
