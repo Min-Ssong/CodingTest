8dd090a255814fd3c6d69c97599b6eb5a1d22c71
origin <2206017@naver.com>
2022-10-01 22:34:40 +0900

    create boj13277

---
+++ BOJ/javaAlgo/boj13277.java
@@ -0,0 +1,15 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class boj13277 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String AB = br.readLine();
+        String[] tempAB = AB.split(" ");
+        BigInteger bi_1 = new BigInteger(tempAB[0]);
+        System.out.print(bi_1.multiply(new BigInteger(tempAB[1])));
+        br.close();
+    }
+}
