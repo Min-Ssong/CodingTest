4a21618342d200042447da034068f87df9c10cec
MS <2206017@naver.com>
2022-10-04 16:44:54 +0900

    create boj15917

---
+++ BOJ/javaAlgo/boj15917.java
@@ -0,0 +1,17 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj15917 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for ( int i = 0; i < N; i++ ) {
+            int a = Integer.parseInt(br.readLine());
+            sb.append( ( (a&(-a)) == a ) ? "1" : "0" ).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
