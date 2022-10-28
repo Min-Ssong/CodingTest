d801bd30d4c2e8c7ca76d9b63643b38bc9ffe4a9
MS <2206017@naver.com>
2022-10-28 14:11:33 +0900

    create SWEA D1_1938

---
+++ SWEA/D1_1938.java
@@ -0,0 +1,13 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class D1_1938 {
+    public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d\n%d\n%d\n%d", a + b, a - b, a * b, a / b);
+        sc.close();
+	}
+}
