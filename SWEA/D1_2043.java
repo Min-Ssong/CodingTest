22b486ce1d70a2dd94b0eac42b819a1435f13945
MS <2206017@naver.com>
2022-10-28 13:50:33 +0900

    create SWEA D1_2043

---
+++ SWEA/D1_2043.java
@@ -0,0 +1,13 @@
+package SWEA;
+
+import java.util.Scanner;
+
+public class D1_2043 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+		int p = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println((p-k) + 1);        
+        sc.close();
+    }   
+}
