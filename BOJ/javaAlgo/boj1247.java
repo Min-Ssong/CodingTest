0bc1e7ee69ef66e81293611398819a97de864d00
origin <2206017@naver.com>
2022-11-29 13:18:04 +0900

    create boj1247

---
+++ BOJ/javaAlgo/boj1247.java
@@ -0,0 +1,23 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+import java.math.BigInteger;
+
+public class boj1247 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for ( int i = 0; i < 3; i++ ) {
+            int loopNum = Integer.parseInt(br.readLine());
+            BigInteger boxBigInt = BigInteger.ZERO;
+            for ( int j = 0; j < loopNum; j++ ) {
+                boxBigInt = boxBigInt.add(new BigInteger(br.readLine()));
+            }
+            int flag = boxBigInt.compareTo(BigInteger.ZERO);
+            sb.append(flag == 0 ? "0\n" : flag > 0 ? "+\n" : "-\n");
+            boxBigInt = BigInteger.ZERO;
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
