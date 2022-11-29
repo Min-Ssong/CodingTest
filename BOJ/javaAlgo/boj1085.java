6af5916166e4fb6ed40c5585358921e59ea6881a
origin <2206017@naver.com>
2022-11-29 11:31:35 +0900

    update boj1085

--- BOJ/javaAlgo/boj1085.java
+++ BOJ/javaAlgo/boj1085.java
@@ -6,13 +6,12 @@ public class boj1085 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String coors = br.readLine();
-        int[] coor = {Integer.parseInt(coors.split(" ")[0]),
-            Integer.parseInt(coors.split(" ")[1]),
-            Integer.parseInt(coors.split(" ")[2]),
-            Integer.parseInt(coors.split(" ")[3])};
-        int wx = coor[2] - coor[0];
-        int hy = coor[3] - coor[1];
-        System.out.println(Integer.min(wx, hy));
+        int x = Integer.parseInt(coors.split(" ")[0]);
+        int y = Integer.parseInt(coors.split(" ")[1]);
+        int w = Integer.parseInt(coors.split(" ")[2]);
+        int h = Integer.parseInt(coors.split(" ")[3]);
+        int result = Integer.min(Integer.min(x, y), Integer.min(Math.abs(w-x), Math.abs(h-y)));
+        System.out.print(result);
         br.close();
     }
 }
