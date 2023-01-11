b631cba421e13084d3ad45053976eed05cf71b07
MS <2206017@naver.com>
2023-01-11 15:26:06 +0900

    create boj10250

---
+++ BOJ/javaAlgo/boj10250.java
@@ -0,0 +1,20 @@
+package BOJ.javaAlgo;
+
+import java.io.*;
+
+public class boj10250 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            String HWN = br.readLine();
+            int H = Integer.parseInt(HWN.split(" ")[0]);
+            int W = Integer.parseInt(HWN.split(" ")[1]);
+            int N = Integer.parseInt(HWN.split(" ")[2]);
+            sb.append(N % H == 0 ? H * 100 + N / H : N % H * 100 + N / H + 1).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
