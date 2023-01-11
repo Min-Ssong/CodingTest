cc110ca9aeb6001e09c072bec97e4bbbad9622d2
MS <2206017@naver.com>
2023-01-11 15:47:16 +0900

    update boj10250

--- BOJ/javaAlgo/B3/boj10250.java
+++ BOJ/javaAlgo/B3/boj10250.java
@@ -6,8 +6,15 @@ public class boj10250 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        
-        System.out.print(sb);
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            String HWN = br.readLine();
+            int H = Integer.parseInt(HWN.split(" ")[0]);
+            int W = Integer.parseInt(HWN.split(" ")[1]);
+            int N = Integer.parseInt(HWN.split(" ")[2]);
+            sb.append(N % H == 0 ? H * 100 + N / H : N % H * 100 + N / H + 1).append("\n");
+        }
+        System.out.println(sb);
         br.close();
     }
 }
