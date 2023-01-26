04d75a076b3684d4e21c5c5040ac8c152485ce31
origin <2206017@naver.com>
2023-01-26 11:21:42 +0900

    create lv0_div

---
+++ programmers/lv0/div.java
@@ -0,0 +1,10 @@
+package programmers.lv0;
+
+public class div {
+    public static void main(String[] args) {
+        int num1 = 3;
+        int num2 = 2;
+        int answer = (int)(((double)num1/num2) * 1000);
+        System.out.println(answer);
+    }
+}
