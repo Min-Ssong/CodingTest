3cb80ba463a54d79ab0f422120616db659732589
origin <2206017@naver.com>
2023-02-17 15:02:18 +0900

    create BOJ S5_23028

---
+++ BOJ/python/greedyAlgorithm/S5_23028.py
@@ -0,0 +1,33 @@
+import time
+start_time = time.time()
+
+# 문제
+N, A, B = map(int, input().split(' '))
+subs = []
+for i in range(0, 10):
+  subs.append(input())  
+
+count = 8 - N
+if A >= 66 and B >= 130:
+  print("Nice")
+else:
+  for i in range(0, count):
+    X = int(subs[i].split(' ')[0])
+    Y = int(subs[i].split(' ')[1])
+    A += X * 3
+    B += X * 3
+    score = 6 - X
+    if score <= Y:
+      B += score * 3
+    else:
+      B += Y * 3
+    score = 6
+
+  if A >= 66 and B >= 130:
+    print("Nice")
+  else:
+    print("Nae ga wae")
+# 문제
+
+end_time = time.time()
+print("time_my:", end_time - start_time)
