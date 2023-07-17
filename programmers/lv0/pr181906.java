8b6f1552b527f53e5c6bf48fa9775f816336bda4
origin <2206017@naver.com>
2023-07-17 21:26:00 +0900

    230717 CodingTest 1

---
+++ programmers/lv0/pr181906.java
@@ -0,0 +1,16 @@
+package programmers.lv0;
+
+public class pr181906 {
+    public int solution(String my_string, String is_prefix) {
+        int a = my_string.indexOf(is_prefix);
+        return a == 0? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        pr181906 p = new pr181906();
+        System.out.println(p.solution("banana", "ban"));
+        System.out.println(p.solution("banana", "nan"));
+        System.out.println(p.solution("banana", "abcd"));
+        System.out.println(p.solution("banana", "bananan"));
+    }
+}
