0455a44a7f953d2185b9885cab5490a8c4573d16
origin <2206017@naver.com>
2023-07-20 21:33:49 +0900

    230720 CodingTest 1

---
+++ programmers/lv0/pr181908.java
@@ -0,0 +1,11 @@
+package programmers.lv0;
+
+public class pr181908 {
+    public int solution(String my_string, String is_suffix) {        
+        return my_string.endsWith(is_suffix) ? 1 : 0;
+    }
+    public static void main(String[] args) {
+        pr181908 pr = new pr181908();
+        pr.solution("banana", "ana");
+    }
+}
