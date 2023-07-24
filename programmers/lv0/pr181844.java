ab55e0a927255c3d41f5d8ba6112eac31293a85f
origin <2206017@naver.com>
2023-07-24 20:57:34 +0900

    230724

---
+++ programmers/lv0/pr181844.java
@@ -0,0 +1,24 @@
+package programmers.lv0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class pr181844 {
+    public int[] solution(int[] arr, int[] delete_list) {
+        List<Integer> result = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < delete_list.length; j++) {
+                if(arr[i] == delete_list[j]) {
+                    arr[i] = 0;
+                    break;
+                }
+            }
+            if(arr[i] != 0) result.add(arr[i]);
+        }
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = result.get(i).intValue();
+        }
+        return answer;
+    }
+}
