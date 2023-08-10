aab521f6bbac26e52ae4c157a394595e176a11e6
origin <2206017@naver.com>
2023-08-10 20:33:49 +0900

    230810 CT PR 181913

---
+++ programmers/lv0/pr181913.java
@@ -0,0 +1,21 @@
+public class pr181913 {
+    class Solution {
+        public String solution(String my_string, int[][] queries) {
+            StringBuilder sb = new StringBuilder(my_string);
+            
+            for(int i = 0; i < queries.length; i++) {
+                int start = queries[i][0];
+                int end = queries[i][1];
+                
+                while(start < end) {
+                    char temp = sb.charAt(start);
+                    sb.setCharAt(start, sb.charAt(end));
+                    sb.setCharAt(end, temp);
+                    start++;
+                    end--;
+                }
+            }   
+            return sb.toString();
+        }
+    }    
+}
