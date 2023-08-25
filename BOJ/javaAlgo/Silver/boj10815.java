413767707cbe2b3b3c76d3d2b26c87dc6b6a675e
origin <2206017@naver.com>
2023-08-25 22:50:43 +0900

    230825 BOJ S5 10815:숫자 카드

---
+++ BOJ/javaAlgo/Silver/boj10815.java
@@ -0,0 +1,28 @@
+package BOJ.javaAlgo.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class boj10815 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuffer sb = new StringBuffer();
+		int N = Integer.parseInt(br.readLine());
+		
+		Map<Integer, Integer> sMap = new HashMap<Integer, Integer>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) sMap.put(Integer.parseInt(st.nextToken()), 1);
+		
+		int M = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			sb.append(sMap.containsKey(num) ? "1" : "0").append(" ");
+		}
+		System.out.println(sb.toString());
+    }
+}
