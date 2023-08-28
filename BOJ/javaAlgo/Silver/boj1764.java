b13ee7ad9f603e93b9fe1d039bf703d4b312a1f9
origin <2206017@naver.com>
2023-08-28 20:20:30 +0900

    230828 CT BOJ S4 1764:듣보잡

---
+++ BOJ/javaAlgo/Silver/boj1764.java
@@ -0,0 +1,35 @@
+package BOJ.javaAlgo.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class boj1764 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String NM = br.readLine();
+		int N = Integer.parseInt(NM.split(" ")[0]);
+		int M = Integer.parseInt(NM.split(" ")[1]);
+		
+		int num = 0;
+		Set<String> tempSet = new TreeSet<String>();
+		Set<String> resultSet = new TreeSet<String>();
+		for(int i = 0; i < N + M; i++){
+			String name = br.readLine();
+			if(i < N){
+				tempSet.add(name);
+			}else if(i >= N && tempSet.contains(name)){
+				resultSet.add(name);
+				num += 1;
+			}
+		}
+		sb.insert(0, num + "\n");
+		for(String str : resultSet){
+			sb.append(str).append("\n");
+		}
+		System.out.println(sb.toString());
+    }
+}
