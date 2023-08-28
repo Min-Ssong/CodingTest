22114c1affb18377d73f5965bce69e9abd6914fd
origin <2206017@naver.com>
2023-08-28 20:10:29 +0900

    230828 CT BOJ S5 1427:소트인사이드

---
+++ BOJ/javaAlgo/Silver/boj1427.java
@@ -0,0 +1,23 @@
+package BOJ.javaAlgo.Silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class boj1427 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split("");
+		List<String> tempArr = new ArrayList<String>();
+		for(int i = 0; i < str.length; i++){
+			tempArr.add(str[i]);
+		}
+		Collections.sort(tempArr, Collections.reverseOrder());
+		for(Object obj : tempArr.toArray()){
+			System.out.print(obj);
+		}
+    }
+}
