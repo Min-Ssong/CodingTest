b9f1f40847330f54adff19db1d8b1d02192e9c8c
origin <2206017@naver.com>
2023-09-19 21:08:27 +0900

    230914 CT PR 1 86491 : 최소직사각형

---
+++ programmers/lv1/pr86491.java
@@ -0,0 +1,28 @@
+package programmers.lv1;
+
+// 230914 CT PR 1 86491 : 최소직사각형
+public class pr86491 {
+	public static void main(String[] args) throws Exception{
+		solution(new int[][]{{60,50},{30,70},{60,30},{80,40}});
+		solution(new int[][]{{10,7},{12,3},{8,15},{14,7},{5,15}});
+		solution(new int[][]{{14,4},{19,6},{6,16},{18,7},{7,11}});
+	}
+	// w compare h and wMax * hMax
+	public static int solution(int[][] sizes){
+		int wMax = Integer.MIN_VALUE;
+		int hMax = Integer.MIN_VALUE;
+		
+		for(int i = 0; i < sizes.length; i++){
+			if(sizes[i][0] > sizes[i][1]){
+				int temp = sizes[i][0];
+				sizes[i][0] = sizes[i][1];
+				sizes[i][1] = temp;
+			}
+			
+			if(sizes[i][0] > wMax) wMax = sizes[i][0];
+			if(sizes[i][1] > hMax) hMax = sizes[i][1];
+		}
+		
+		return wMax * hMax;
+	}
+}
