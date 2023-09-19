55e154196a0bf8cfe118b56432e3cf56f3c4e6b9
origin <2206017@naver.com>
2023-09-19 20:58:10 +0900

    230919 CT BOJ B3:그대로 출력하기 2

---
+++ BOJ/javaAlgo/Bronze/boj11719.java
@@ -0,0 +1,14 @@
+package BOJ.javaAlgo.Bronze;
+
+import java.io.*;
+
+// 230919 CT BOJ B3:그대로 출력하기 2
+public class boj11719{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        while((str= br.readLine()) != null){
+            System.out.println(str);
+        }
+    }
+}
