daebcd6ec79b210c42623b30974949802dea20c9
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:51:23 +0900

    [Bronze IV] Title: Boiling Water, Time: 140 ms, Memory: 15800 KB -BaekjoonHub

---
+++ 백준/Bronze/21612. Boiling Water/Boiling Water.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int P = (Integer.parseInt(br.readLine())*5) - 400;
+        if ( P == 100 ) {
+            System.out.print(P + "\n0");
+        } else {
+            System.out.print((P > 100) ? P + "\n-1" : P + "\n1");
+        }
+        br.close();        
+    }
+}
