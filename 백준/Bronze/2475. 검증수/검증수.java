2d46140c5593e513a98234c07f7c2d2077737e2f
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:06:46 +0900

    [Bronze V] Title: 검증수, Time: 212 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,19 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputStr = sc.nextLine();
+        double temp = 0.0;
+        for( int i = 0; i < 5; i++ ) {
+            int num = Integer.parseInt(inputStr.trim().split(" ")[i]);
+            temp += Math.pow(num, 2);
+        }
+        
+        DecimalFormat df = new DecimalFormat("0.#");
+        System.out.println(df.format(temp%10));
+
+        sc.close();
+    }
+}
