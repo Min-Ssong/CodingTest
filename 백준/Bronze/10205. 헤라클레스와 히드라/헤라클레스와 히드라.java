a91c22acc5a0c36a3c69cdf21295fa0dd4358571
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:32:34 +0900

    [Bronze III] Title: 헤라클레스와 히드라, Time: 140 ms, Memory: 14684 KB -BaekjoonHub

---
+++ 백준/Bronze/10205. 헤라클레스와 히드라/헤라클레스와 히드라.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int K = Integer.parseInt(br.readLine());
+        for ( int i = 0; i < K; i++ ) {
+            sb.append("Data Set "+(i+1)+":\n");
+            int head = Integer.parseInt(br.readLine());
+            String behaviors = br.readLine();
+            for (String str : behaviors.split("")) {
+                if ( head != 0 ) {
+                    head += str.equals("b") ? -1 : 1;
+                } else {
+                    break;
+                }
+            }
+            sb.append((head == 0) ? 0 : head);
+            sb.append("\n\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
