ef93e356c57ad90963f1a1e28a9eff1d3dc0539a
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:36:29 +0900

    [Bronze III] Title: 모음의 개수, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        char[] ch = word.toCharArray();
+        int count = 0;
+        for ( int i = 0; i < ch.length; i++ ) {
+            int num = (int)ch[i];
+            if ( num == 97 || num == 101 || num == 105 || num == 111 || num == 117 ) {
+                count += 1;
+            }
+        }
+        System.out.print(count);
+        br.close();
+    }
+}
