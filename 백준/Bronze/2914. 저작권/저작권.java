25c102a054e40134727bd6c20681a4ddfadd874c
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:14:28 +0900

    [Bronze III] Title: 저작권, Time: 128 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String allInput = br.readLine();
+        int A = Integer.parseInt(allInput.split(" ")[0]);
+        int I = Integer.parseInt(allInput.split(" ")[1]);
+        System.out.println((A * (I - 1)) + 1);
+        br.close();
+    }
+}
