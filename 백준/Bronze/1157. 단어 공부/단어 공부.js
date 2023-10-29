ab6b4cb1bb93dc9be3a238165a2104b68c825064
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:00:00 +0900

    [Bronze I] Title: 단어 공부, Time: 468 ms, Memory: 53336 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.js
@@ -0,0 +1,26 @@
+const input = require("fs").readFileSync('/dev/stdin').toString().trim().toUpperCase().split('').sort();
+let countArr = new Array(26).fill(0);
+let count = 0;
+let max = 0;
+let wordAscii = 0;
+for(let i=0; i<input.length; i++){
+    let wordAlpha = input[i].charCodeAt(0)-65;
+    countArr[wordAlpha]++;
+}
+for(let i=0; i<countArr.length; i++){
+    if(countArr[i] >= max){
+        if(countArr[i] === max){
+            count++;
+        }else{
+            count = 0;
+        }
+
+        max = countArr[i];
+        wordAscii = i;
+    }
+}
+if(count === 0){
+    console.log(String.fromCharCode(wordAscii+65));
+}else{
+    console.log('?');
+}
