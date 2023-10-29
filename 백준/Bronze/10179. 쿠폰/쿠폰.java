ab1556a6f397b0f49a1710ef43abf4a805c51930
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:32:28 +0900

    [Bronze III] Title: 쿠폰, Time: 136 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Bronze/10179. 쿠폰/쿠폰.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for ( int i = 0; i < T; i++ ) {
+            Double price = Double.parseDouble(br.readLine());
+            sb.append(String.format("$%.2f", price * 0.8)).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
