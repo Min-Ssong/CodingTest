578402d8af6e3ab78286032f8c6075a1808958c9
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:38:54 +0900

    [Bronze III] Title: 지속, Time: 124 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Bronze/11648. 지속/지속.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String N = br.readLine();
+        int count = 0;
+        while ( Integer.parseInt(N) >= 10 ) {
+            String[] num = N.split("");
+            int sum = 1;
+            for (String str : num) {
+                sum *= Integer.parseInt(str);
+            }
+            N = String.valueOf(sum);
+            count += 1;
+        }
+        sb.append(count);
+        System.out.println(sb);
+        br.close();
+    }
+}
