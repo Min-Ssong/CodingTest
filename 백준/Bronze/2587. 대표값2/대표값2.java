9adfcdcc0d13683311a7cb24334bf8fee6a2b151
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:09:31 +0900

    [Bronze II] Title: 대표값2, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		List<Integer> tempArr = new ArrayList<Integer>();
+		int sum = 0;
+		for(int i = 0; i < 5; i++){
+			int num = Integer.parseInt(br.readLine());
+			tempArr.add(num);
+			sum += num;
+		}
+		Collections.sort(tempArr);
+		System.out.println(sum / tempArr.size());
+		System.out.println(tempArr.get(tempArr.size() / 2));
+    }
+}
