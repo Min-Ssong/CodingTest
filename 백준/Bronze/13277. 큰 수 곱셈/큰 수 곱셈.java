ac5fd1c0583f24948aa9baafaa6c95ca3d0126fc
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:41:00 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6108 ms, Memory: 407816 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String AB = br.readLine();
+        String[] tempAB = AB.split(" ");
+        BigInteger bi_1 = new BigInteger(tempAB[0]);
+        System.out.print(bi_1.multiply(new BigInteger(tempAB[1])));
+        br.close();
+    }
+}
