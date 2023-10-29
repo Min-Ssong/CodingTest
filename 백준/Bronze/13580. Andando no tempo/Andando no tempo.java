0bab43a46919bffef66b4a940d3b95b0feacde32
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:41:26 +0900

    [Bronze IV] Title: Andando no tempo, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/13580. Andando no tempo/Andando no tempo.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputs = br.readLine();
+        int[] time = { Integer.parseInt(inputs.split(" ")[0]),
+                       Integer.parseInt(inputs.split(" ")[1]),
+                       Integer.parseInt(inputs.split(" ")[2])};
+        Arrays.sort(time);
+        if ( time[0] == time[1] || time[0] == time[2] || time[1] == time[2] ) {
+            System.out.print("S");
+        } else {
+            System.out.println(( time[0] + time[1]  == time[2] ) ? "S" : "N");
+        }
+
+        br.close();
+    }
+}
