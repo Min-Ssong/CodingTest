6a07333bcc1db692012c044e3c6f5f3910c7a564
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:15:23 +0900

    [Bronze III] Title: 네 번째 수, Time: 132 ms, Memory: 14480 KB -BaekjoonHub

---
+++ 백준/Bronze/2997. 네 번째 수/네 번째 수.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String allInput = br.readLine();
+        int[] nums = {
+            Integer.parseInt(allInput.split(" ")[0]),
+            Integer.parseInt(allInput.split(" ")[1]),
+            Integer.parseInt(allInput.split(" ")[2])
+        };
+        Arrays.sort(nums);
+        if ( nums[1] - nums[0] == nums[2] - nums[1] ) {
+            System.out.print((nums[2] - nums[1]) + nums[2]);
+        } else {
+            System.out.printf("%d",
+                ( nums[1] - nums[0] > nums[2] - nums[1] ) 
+                    ? (nums[2] - nums[1]) + nums[0]
+                    : (nums[1] - nums[0]) + nums[1]
+            );
+        }
+        
+        br.close();
+    }
+}
