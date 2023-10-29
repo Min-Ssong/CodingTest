3e5daaf9e9ac0f34a0cca69278825fd4a4eb2713
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:49:22 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 120 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        int[] inputs = { Integer.parseInt(input.split(" ")[0]), 
+            Integer.parseInt(input.split(" ")[1]), 
+            Integer.parseInt(input.split(" ")[2])
+        };
+        Arrays.sort(inputs);
+        System.out.print(inputs[1] + inputs[2]);
+        br.close();
+    }
+}
