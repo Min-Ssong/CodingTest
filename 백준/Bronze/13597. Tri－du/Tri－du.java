1e4ac71a0f3c9faabe7623a28ba43db257398440
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:41:33 +0900

    [Bronze IV] Title: Tri-du, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/13597. Tri－du/Tri－du.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String inputs = br.readLine();
+        int A = Integer.parseInt(inputs.split(" ")[0]);
+        int B = Integer.parseInt(inputs.split(" ")[1]);
+        System.out.println( ( A == B ) ? A : ( A > B ) ? A : B );
+        br.close();
+    }
+}
