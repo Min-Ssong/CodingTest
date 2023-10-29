3bc2b2fe681694fe1f81779e5eb84b726685c7b9
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:23:28 +0900

    [Bronze V] Title: 콜센터, Time: 116 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = 
+        "     /~\\\n"+
+        "    ( oo|\n"+
+        "    _\\=/_\n"+
+        "   /  _  \\\n"+
+        "  //|/.\\|\\\\\n"+
+        " ||  \\ /  ||\n"+
+        "============\n"+
+        "|          |\n"+
+        "|          |\n"+
+        "|          |\n";
+        System.out.println(str);
+    }
+}
