dda8389dc5964bc078a1df3827438c49c8ff279c
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:06:16 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 184 ms, Memory: 15128 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int num = N * 2 - 1;
+        for ( int i = 0; i < N; i++ ) {
+            System.out.printf("%s%s\n",
+                " ".repeat(i),
+                "*".repeat(num - (i*2))
+            );
+        }
+        for ( int i = N - 2; i >= 0; i-- ) {
+            System.out.printf("%s%s\n",
+                " ".repeat(i),
+                "*".repeat(num - (i*2))
+            );
+        }
+        br.close();
+    }
+}
