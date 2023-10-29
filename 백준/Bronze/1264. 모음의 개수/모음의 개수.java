53d58f6d66dc805eb1a5be41a17bf86967769a29
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:00:40 +0900

    [Bronze IV] Title: 모음의 개수, Time: 220 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            String str = sc.nextLine().toLowerCase();
+
+            if( str.equals("#") ){
+                sc.close();
+                break;
+            } else {
+                System.out.printf("%d\n",
+                countChar(str, 'a')
+                + countChar(str, 'e')
+                + countChar(str, 'i')
+                + countChar(str, 'o')
+                + countChar(str, 'u'));
+            }
+        }
+    }
+    public static int countChar(String str, char ch) {
+        return str.length() - str.replace(String.valueOf(ch), "").length();
+    }
+}
