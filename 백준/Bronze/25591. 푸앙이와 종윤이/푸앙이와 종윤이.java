ba1cf0e8349075158445460738a6a3b6840d6672
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:55:58 +0900

    [Bronze IV] Title: 푸앙이와 종윤이, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/25591. 푸앙이와 종윤이/푸앙이와 종윤이.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int a = 100 - A;
+        int b = 100 - B;
+        int c = 100 - (a + b);
+        int d = a * b;
+        int q = d / 100;
+        int r = d % 100;
+
+        sb.append(a).append(" ");
+        sb.append(b).append(" ");
+        sb.append(c).append(" ");
+        sb.append(d).append(" ");
+        sb.append(q).append(" ");
+        sb.append(r).append("\n");
+        sb.append(q + c).append(" ");
+        sb.append(r);
+
+        System.out.print(sb);
+        br.close();
+    }
+}
