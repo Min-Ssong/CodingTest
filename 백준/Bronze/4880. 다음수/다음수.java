9dcc2472b25a6febd2260bc1abfa4055bc6b9410
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:20:45 +0900

    [Bronze III] Title: 다음수, Time: 140 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/4880. 다음수/다음수.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while ( true ) {
+            String apgp = br.readLine();
+            if ( apgp.equals("0 0 0") ) {
+                System.out.print(sb);
+                break;
+            }
+
+            StringTokenizer st = new StringTokenizer(apgp);
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+
+            if ( C - B == B - A ) {
+                sb.append("AP ").append(String.valueOf((C-B) + C)).append("\n");
+            } else if ( C / B == B / A ) {
+                sb.append("GP ").append(String.valueOf((C/B) * C)).append("\n");
+            }
+        }
+        br.close();
+    }
+}
