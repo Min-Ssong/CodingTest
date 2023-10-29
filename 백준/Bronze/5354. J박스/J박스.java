f0b8ac9a5257db6130ee9f2994e3a8850cb217fd
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:23:38 +0900

    [Bronze III] Title: J박스, Time: 152 ms, Memory: 16152 KB -BaekjoonHub

---
+++ 백준/Bronze/5354. J박스/J박스.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String result  = "";
+        for ( int i = 0; i < N; i++ ) {
+            int boxSize = Integer.parseInt(br.readLine());
+            for ( int j = 0; j < boxSize; j++ ) {
+                if ( j == 0 || j == boxSize - 1 ) {
+                    result += "#".repeat(boxSize) + "\n";
+                } else {
+                    result += "#" + "J".repeat(boxSize - 2) + "#\n";
+                }
+            }
+            result += "\n";
+        }
+        System.out.print(result);
+        br.close();
+    }
+}
