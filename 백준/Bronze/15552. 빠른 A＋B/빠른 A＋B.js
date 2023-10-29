f6b1a250a16eb4f54a351628075e70b05e0b56a4
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:44:45 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1344 ms, Memory: 102616 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const r = readline.createInterface({ 
+    input:process.stdin, 
+    output:process.stdout 
+});
+
+let answer = "";
+r.on('line', (input) => {
+    let tmp = input.split(" ");
+    if(tmp.length === 2){
+        let A = parseInt(tmp[0]);
+        let B = parseInt(tmp[1])
+        answer += A+B+'\n';
+    }
+}).on('close', function(){
+    console.log(answer);
+    process.exit();
+});
