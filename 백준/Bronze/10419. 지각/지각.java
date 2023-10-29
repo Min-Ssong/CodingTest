1d1c32baf695a2df5ef254e61970442340ceafbe
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:32:53 +0900

    [Bronze III] Title: 지각, Time: 120 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/10419. 지각/지각.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        int num = 0;
+        for ( int i = 0; i < T; i++ ) {
+            int d = Integer.parseInt(br.readLine());
+            int j = 1;
+            while ( true ) {
+                if ( d >= (j + Math.pow(j, 2)) ) {
+                    num = j;
+                } else {
+                    break;
+                }
+                j += 1;
+            }
+            sb.append(num).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
