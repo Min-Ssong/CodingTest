195ecb9e2e4812d23a73e353a989cf0e95808754
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:26:45 +0900

    [Bronze III] Title: Another Cow Number Game, Time: 124 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/6190. Another Cow Number Game/Another Cow Number Game.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Long N = Long.parseLong(br.readLine());
+        int score = 0;
+        while ( N != 1 ) {            
+            if ( N % 2 == 0 ) {
+                N = N / 2;
+            } else {
+                N = 3 * N + 1;
+            }
+            score++;
+        }
+        System.out.print(score);
+        br.close();
+    }
+}
