cd1dac92f6c9a4efef289057adb91756d094df14
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:23:09 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = ".  .   .\n"+
+        "|  | _ | _. _ ._ _  _\n"+
+        "|/\\|(/.|(_.(_)[ | )(/.";
+        System.out.println(str);
+    }
+}
