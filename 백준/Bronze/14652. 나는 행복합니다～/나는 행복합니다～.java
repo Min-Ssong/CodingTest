2421dc5fd78868d4b31284a09ebbab9d97d90edb
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:43:03 +0900

    [Bronze V] Title: 나는 행복합니다~, Time: 456 ms, Memory: 17436 KB -BaekjoonHub

---
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String nmk = br.readLine();
+        int n = Integer.parseInt(nmk.split(" ")[0]);
+        int m = Integer.parseInt(nmk.split(" ")[1]);
+        int k = Integer.parseInt(nmk.split(" ")[2]);
+        int num = 0;
+        for ( int i = 0; i < n; i++ ) {
+            for ( int j = 0; j < m; j++ ) {
+                if ( k == num ) {
+                    System.out.println(i + " " + j);
+                    System.exit(0);
+                }
+                num += 1;
+            }
+        }
+        br.close();
+    }
+}
