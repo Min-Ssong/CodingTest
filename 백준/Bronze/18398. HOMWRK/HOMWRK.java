e0be709913774d13aceea66d47c73fcf5141b313
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:48:52 +0900

    [Bronze IV] Title: HOMWRK, Time: 184 ms, Memory: 17448 KB -BaekjoonHub

---
+++ 백준/Bronze/18398. HOMWRK/HOMWRK.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String result = "";
+        int T = Integer.parseInt(br.readLine());
+        for ( int i = 0; i < T; i++ ){
+            int N = Integer.parseInt(br.readLine());
+            for ( int j = 0; j < N; j++ ) {
+                String allInput = br.readLine();
+                int A = Integer.parseInt(allInput.split(" ")[0]);
+                int B = Integer.parseInt(allInput.split(" ")[1]);
+                result += A+B + " " + A*B + "\n";
+            }
+        }
+        System.out.print(result);
+        br.close();
+    }
+}
