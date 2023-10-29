993fdd15910202c9baca3e74e1e5d0e0c705791c
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:39:00 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 732 ms, Memory: 54784 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[N][2];
+		for(int i = 0; i < N; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[i] = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+		}
+		
+		Arrays.sort(arr, (e1, e2) -> {
+				if(e1[0] == e2[0]) {
+					return e1[1] - e2[1];
+				} else {
+					return e1[0] - e2[0];
+				}
+			});
+		
+		for(int i = 0; i < arr.length; i++){
+			sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
