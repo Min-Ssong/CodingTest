b42a06d76354a6f33b72a1d1c00968ec9ed970ff
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:19:27 +0900

    [Bronze III] Title: No Brainer, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String result = "";
+        for ( int i = 0; i < N; i++ ) {
+            String brains = br.readLine();
+            int a = Integer.parseInt(brains.split(" ")[0]);
+            int b = Integer.parseInt(brains.split(" ")[1]);
+            result += ( a < b ) ? "NO BRAINS\n" : "MMM BRAINS\n";
+        }
+        System.out.print(result);
+        br.close();
+    }
+}
