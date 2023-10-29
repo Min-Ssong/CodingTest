c36e8c422e87e0076c6b6783d3a8bdcc5d604790
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:12:39 +0900

    [Bronze III] Title: 삼각 김밥, Time: 140 ms, Memory: 14600 KB -BaekjoonHub

---
+++ 백준/Bronze/2783. 삼각 김밥/삼각 김밥.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String seven25 = br.readLine();
+        Double sevenX = Double.parseDouble(seven25.split(" ")[0]);
+        Double sevenY = Double.parseDouble(seven25.split(" ")[1]);
+        int T = Integer.parseInt(br.readLine());
+        Double min = 9999.99;
+        for ( int i = 0; i < T; i++ ) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            Double X = Double.parseDouble(st.nextToken());
+            Double Y = Double.parseDouble(st.nextToken());
+            Double temp = (1 / Y) * X * 1000;
+            min = ( min > temp ) ? temp : min;
+        }
+        Double sevenValue = (1 / sevenY) * sevenX * 1000;
+        System.out.printf("%.2f", (min > sevenValue) ? sevenValue : min);
+        br.close();       
+    }
+}
