8d7c5da96ab9400650891bc86f28bdc7e962acd6
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:12:00 +0900

    [Bronze IV] Title: 세수정렬, Time: 144 ms, Memory: 14668 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String nums = br.readLine();
+        int[] num = {Integer.parseInt(nums.split(" ")[0]), Integer.parseInt(nums.split(" ")[1]), Integer.parseInt(nums.split(" ")[2])};
+        Arrays.sort(num);
+        for (int i : num) {
+            System.out.print(i + " ");
+        }
+        br.close();
+    }
+}
