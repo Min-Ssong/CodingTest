84db2bc1aa755b00aa5ec73b671564ead6502e5e
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:57:06 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long N = Long.parseLong(br.readLine());
+		System.out.println(facto(N));
+    }
+
+    public static long facto(long num){
+		if(num <= 1){
+			return 1;
+		} else {
+			return num * facto(num - 1);
+		}
+	}
+}
