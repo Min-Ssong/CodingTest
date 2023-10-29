6ed8d9179090c95ac8719b3705a25b3f5c90a903
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:42:24 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 120 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String money = br.readLine();
+        int a = Integer.parseInt(money.split(" ")[0]);
+        int b = Integer.parseInt(money.split(" ")[1]);
+        int chickens = Integer.parseInt(br.readLine()) * 2;
+        System.out.print( ( a + b >= chickens ) ? (a + b) - chickens : a + b );
+
+        br.close();
+    }
+}
