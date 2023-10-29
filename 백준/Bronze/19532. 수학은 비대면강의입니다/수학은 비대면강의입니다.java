b305e6a9770e5bf945740ea0c2309bfd30b1a0b2
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:49:45 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 208 ms, Memory: 16564 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int a = Integer.parseInt(str.split(" ")[0]);
+		int b = Integer.parseInt(str.split(" ")[1]);
+		int c = Integer.parseInt(str.split(" ")[2]);
+		int d = Integer.parseInt(str.split(" ")[3]);
+		int e = Integer.parseInt(str.split(" ")[4]);
+		int f = Integer.parseInt(str.split(" ")[5]);
+		
+		for(int x = -999; x <= 999; x++){
+			for(int y = -999; y <= 999; y++){
+				if(a * x + b * y == c && d * x + e * y == f){
+					System.out.println(x + " " + y);
+				}
+			}
+		}
+    }
+}
