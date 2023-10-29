26c0324a829d0818d2e7ca929d7b7e188e627c3b
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:49:35 +0900

    [Bronze IV] Title: The Walking Adam, Time: 264 ms, Memory: 19028 KB -BaekjoonHub

---
+++ 백준/Bronze/18698. The Walking Adam/The Walking Adam.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = Integer.parseInt(sc.nextLine());
+        String result = "";
+        for ( int i = 0; i < len; i++ ) {
+            String upAndDown = sc.nextLine();
+            if ( upAndDown.indexOf("D") == -1 ) {
+                result += String.valueOf(upAndDown.length()) + "\n";
+            } else {
+                result += upAndDown.substring(0, upAndDown.indexOf("D")).length() + "\n";
+            }
+        }
+        System.out.print(result);
+        sc.close();
+    }
+}
