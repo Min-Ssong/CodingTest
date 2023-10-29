90ef33a90f5444355d373b1176f3e1f08f092013
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:46:53 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String allInput = br.readLine();
+        int N = Integer.parseInt(allInput.split(" ")[0]);
+        int M = Integer.parseInt(allInput.split(" ")[1]);
+        int K = Integer.parseInt(allInput.split(" ")[2]);
+        int result = 0;
+        
+        result += ( M <= K ) ? M : K;
+        result += (N-M <= N-K) ? N-M : N-K;
+        System.out.print(result);
+
+        br.close();
+    }
+}
