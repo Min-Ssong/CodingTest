0818f5a6aae4aaff558430b0e3f6c845c79a4e44
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:48:12 +0900

    [Bronze IV] Title: Counting Clauses, Time: 208 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/17903. Counting Clauses/Counting Clauses.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        String temp = "";
+        for ( int i = 0; i <= M; i++ ) {
+            temp += sc.nextLine();
+        }
+        System.out.println( ( M >= 8 ) ? "satisfactory" : "unsatisfactory" );
+        sc.close();
+    }
+}
