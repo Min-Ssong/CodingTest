b99f47d07aa15f435159b1f9ced5f9f0e21319bb
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:02:49 +0900

    [Silver II] Title: 스택 수열, Time: 392 ms, Memory: 30368 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        int count = 0;
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(!stack.isEmpty()){
+                if(stack.peek() < num){
+                    for(int j = count + 1; j <= num; j++){
+                        stack.push(j);
+                        sb.append("+").append("\n");
+                    }
+                    count = num;
+                    
+                    stack.pop();
+                    sb.append("-").append("\n");
+                }
+                
+                if(stack.peek() >= num){
+                    if(stack.peek() != num){
+                        sb.setLength(0);
+                        sb.append("NO");
+                        break;
+                    }
+                    
+                    for(int j = stack.peek(); j >= num; j--){
+                        stack.pop();
+                        sb.append("-").append("\n");
+                    }
+                }
+            } else{
+                for(int j = count + 1; j <= num; j++){
+                    stack.push(j);
+                    sb.append("+").append("\n");
+                }
+                count = num;
+                
+                stack.pop();
+                sb.append("-").append("\n");
+            }
+        }
+        System.out.println(sb.toString());
+        br.close();
+    }
+}
