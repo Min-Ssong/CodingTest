ffc9dff4d9e1a45af1b25824f706a30fe51a8c6b
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:02:09 +0900

    [Bronze III] Title: 공, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String ball = "1";
+        int T = Integer.parseInt(br.readLine());
+        for ( int i = 0; i < T; i++ ){
+            String AB = br.readLine();
+            String A = AB.split(" ")[0];
+            String B = AB.split(" ")[1];
+            if ( ball.equals(A) ) {
+                ball = B;
+            } else if ( ball.equals(B) ){
+                ball = A;
+            }
+        }
+        System.out.print(ball);
+        br.close();
+    }
+}
