ea15eb6737b5d7314b96278f86f543d41959acc2
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:14:38 +0900

    [Bronze II] Title: 음계, Time: 220 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int[] asc = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] desc = {8, 7, 6, 5, 4, 3, 2, 1};
+        int[] splitInt = new int[8];
+        for( int i = 0; i < 8; i++ ) {
+            splitInt[i] = Integer.parseInt(str.trim().split(" ")[i]);
+        }
+
+        if( Arrays.equals(splitInt, asc) ) {
+            System.out.println("ascending");
+        }else if( Arrays.equals(splitInt, desc) ) {
+            System.out.println("descending");
+        }else{
+            System.out.println("mixed");
+        }
+        
+        sc.close();
+    }
+}
