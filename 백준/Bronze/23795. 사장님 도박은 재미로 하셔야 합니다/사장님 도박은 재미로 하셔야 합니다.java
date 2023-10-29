ab2847c7e9f8713d794a053e594d0675b4990131
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:53:01 +0900

    [Bronze IV] Title: 사장님 도박은 재미로 하셔야 합니다, Time: 136 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Bronze/23795. 사장님 도박은 재미로 하셔야 합니다/사장님 도박은 재미로 하셔야 합니다.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
+        while ( true ) {
+            int N = Integer.parseInt(br.readLine());
+            if ( N == -1 ) {
+                System.out.print(result);
+                break;
+            }
+            result += N;
+        }
+        br.close();
+    }
+}
