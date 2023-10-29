9db52700e8e3474c9aa3fa4d1561d98e2a8967ee
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:18:48 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 164 ms, Memory: 16352 KB -BaekjoonHub

---
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        String result = "";
+        for ( int i = 0; i < T; i++ ) {
+            String str = br.readLine();
+            String[] strArr = str.split("");
+            result += strArr[0].toUpperCase() + str.substring(1, str.length()) + "\n";
+        }
+        System.out.print(result);
+        br.close();
+    }
+}
