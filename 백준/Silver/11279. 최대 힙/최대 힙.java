d8c2ccf3ce7e1a5971ea7785a88b9f243071bdc5
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:37:58 +0900

    [Silver II] Title: 최대 힙, Time: 352 ms, Memory: 26192 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> preQueue = new PriorityQueue<Integer>(Collections.reverseOrder());
+        
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 0; i < N; i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num == 0){
+                sb.append(preQueue.isEmpty() ? 0 : preQueue.poll()).append("\n");
+            } else {
+                preQueue.offer(num);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
