8bc3ea826057e6c5c4865d4729ec7f6ee813620a
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:32:02 +0900

    [Bronze III] Title: 전자레인지, Time: 144 ms, Memory: 15968 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = 300;
+        int B = 60;
+        int C = 10;
+        int[] result = {0, 0, 0};
+
+        int T = Integer.parseInt(br.readLine());
+        if ( T % 10 == 0 ) {
+            while ( T > 0 ) {
+                if ( T >= A ) {
+                    T = T - A;
+                    result[0] += 1;
+                } else if ( T >= B ) {
+                    T = T - B;
+                    result[1] += 1;
+                } else if ( T >= C ) {
+                    T = T - C;
+                    result[2] += 1;
+                }
+            }
+            for (int i : result) {
+                bw.write(i + " ");
+            }
+        } else {
+            bw.write(String.valueOf(-1));
+        }
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
