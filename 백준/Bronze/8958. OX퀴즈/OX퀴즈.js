79f19546cb12c3f877793b7a64b5f379e0ae2bcc
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:29:43 +0900

    [Bronze II] Title: OX퀴즈, Time: 136 ms, Memory: 9812 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,21 @@
+let input = require("fs").readFileSync('/dev/stdin').toString().trim();
+let originArr = input.split('\n');
+let count = 0; result = 0;
+originArr.shift();
+for(let i=0; i<originArr.length; i++){
+    let tmp = originArr[i];
+    for(let j=0; j<tmp.length; j++){
+        if(tmp[j].search('O') == 0){
+            if(count == 0)
+                count = 1;
+            else
+                count += 1;
+        }else
+            count = 0;
+        result += count;
+    }
+    console.log(result);
+
+    count = 0;
+    result = 0;
+}
