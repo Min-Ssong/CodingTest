e66acee39ed5b8305bdd2aa4f24332411fa63142
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:45:10 +0900

    [Bronze III] Title: 명장 남정훈, Time: 120 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/15734. 명장 남정훈/명장 남정훈.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String LRAs = br.readLine();
+        String[] LRA = LRAs.split(" ");
+        int L = Integer.parseInt(LRA[0]);
+        int R = Integer.parseInt(LRA[1]);
+        int A = Integer.parseInt(LRA[2]);
+
+        int sum = L + R + A;
+		int temp = A - Math.abs(L - R);
+
+		System.out.println(temp >= 0 ? sum - temp % 2 : sum - Math.abs(temp));
+        br.close();
+    }
+}
