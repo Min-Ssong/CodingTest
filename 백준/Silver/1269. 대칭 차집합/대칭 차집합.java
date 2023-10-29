dc3da9b26a0a9fbe58f1b3ac1febf3cf63f6852d
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:00:50 +0900

    [Silver IV] Title: 대칭 차집합, Time: 1052 ms, Memory: 102084 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] AB = br.readLine().split(" ");
+		int A = Integer.parseInt(AB[0]);
+		int B = Integer.parseInt(AB[1]);
+		
+		Set<Integer> aSet = new HashSet<Integer>();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < A; i++){
+			aSet.add(Integer.parseInt(st.nextToken()));	
+		}
+		
+		Set<Integer> bSet = new HashSet<Integer>();
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < B; i++){
+			bSet.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		Set<Integer> diffAB = new HashSet<Integer>();
+		diffAB.addAll(aSet);
+		
+		Set<Integer> diffBA = new HashSet<Integer>();
+		diffBA.addAll(bSet);
+		
+		diffAB.removeAll(diffBA);
+		bSet.removeAll(aSet);
+		System.out.println(diffAB.size() + bSet.size());
+    }
+}
