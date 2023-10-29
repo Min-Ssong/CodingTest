c949d0b0ce09a8f7fdb0d1b22d2f761d44475af8
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:43:09 +0900

    [Bronze III] Title: 조교는 새디스트야!!, Time: 184 ms, Memory: 16792 KB -BaekjoonHub

---
+++ 백준/Bronze/14656. 조교는 새디스트야！！/조교는 새디스트야！！.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int count = 0;
+        for ( int i = 1; i <= T; i++ ) {
+            int num = Integer.parseInt(st.nextToken());
+            count += ( i == num ) ? 0 : 1;
+        }
+        System.out.print(count);
+        br.close();
+    }
+}
