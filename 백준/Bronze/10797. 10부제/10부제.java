20d2f74affe58a55d240bedfdad6f4b33f86a74b
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:33:44 +0900

    [Bronze IV] Title: 10부제, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int day = Integer.parseInt(br.readLine());
+        String[] carNum = br.readLine().split(" ");
+        int count = 0;
+
+        for ( int i = 0; i < carNum.length; i++ ) {
+            if( day == Integer.parseInt(carNum[i]) ){
+                count++;
+            }
+        }
+        bw.write(String.valueOf(count));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
