43f3cdc08fd5b78c09a56bca86d9bd06a117e084
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:52:11 +0900

    [Bronze IV] Title: 金平糖 (Konpeito), Time: 124 ms, Memory: 14236 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/22015. 金平糖 （Konpeito）/金平糖 （Konpeito）.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+        int[] num = new int[3];
+        for ( int i = 0; i < inputs.length; i++ ) {
+            num[i] = Integer.parseInt(inputs[i]);
+        }
+        Arrays.sort(num);
+        System.out.print((num[2] - num[0]) + (num[2] - num[1]));
+        br.close();
+    }
+}
