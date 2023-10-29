f997a3fbcd27767406924b2aa1e1a4d341a7e10e
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:47:23 +0900

    [Bronze IV] Title: Winning Score, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

---
+++ 백준/Bronze/17009. Winning Score/Winning Score.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A_3 = Integer.parseInt(br.readLine());
+        int A_2 = Integer.parseInt(br.readLine());
+        int A_1 = Integer.parseInt(br.readLine());
+
+        int B_3 = Integer.parseInt(br.readLine());
+        int B_2 = Integer.parseInt(br.readLine());
+        int B_1 = Integer.parseInt(br.readLine());
+
+        int resultA = (A_3 * 3) + (A_2 * 2) + A_1;
+        int resultB = (B_3 * 3) + (B_2 * 2) + B_1;
+        if ( resultA == resultB ) {
+            System.out.print("T");
+        } else {
+            System.out.print( (resultA > resultB) ? "A" : "B" );
+        }
+        br.close();
+    }
+}
