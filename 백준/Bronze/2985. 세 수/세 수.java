8b06c973ea61c47e7122600dbd499bcb0a2624c2
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:15:14 +0900

    [Bronze III] Title: 세 수, Time: 132 ms, Memory: 14480 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String allInput = br.readLine();
+        int A = Integer.parseInt(allInput.split(" ")[0]);
+        int B = Integer.parseInt(allInput.split(" ")[1]);
+        int C = Integer.parseInt(allInput.split(" ")[2]);
+
+        if ( A + B == C ) {
+            System.out.printf("%d+%d=%d", A, B, C);
+        } else if ( A - B == C ) {
+            System.out.printf("%d-%d=%d", A, B, C);
+        } else if ( A * B == C ) {
+            System.out.printf("%d*%d=%d", A, B, C);
+        } else if ( A / B == C ) {
+            System.out.printf("%d/%d=%d", A, B, C);
+        } else if ( A == B + C ) {
+            System.out.printf("%d=%d+%d", A, B, C);
+        } else if ( A == B - C ) {
+            System.out.printf("%d=%d-%d", A, B, C);
+        } else if ( A == B * C ) {
+            System.out.printf("%d=%d*%d", A, B, C);
+        } else if ( A == B / C ) {
+            System.out.printf("%d=%d/%d", A, B, C);
+        }
+        
+        br.close();
+    }
+}
