342de5453ac6962aea5e8fdcff8cf0c64d3893c9
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:27:24 +0900

    [Bronze III] Title: Golf, Time: 136 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/6437. Golf/Golf.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int i = 1;
+        while ( true ) {
+            String ps = br.readLine();
+            if ( ps.equals("0 0") ) {
+                System.out.print(sb);
+                break;
+            }
+            
+            int p = Integer.parseInt(ps.split(" ")[0]);
+            int s = Integer.parseInt(ps.split(" ")[1]);
+            sb.append("Hole #" + i + "\n");
+            if ( s == 1 ) {
+                sb.append("Hole-in-one.");
+            } else {
+                int num = s - p;
+                if ( num >= 2) {
+                    sb.append("Double Bogey.");
+                } else if ( num == 1 ) {
+                    sb.append("Bogey.");
+                } else if ( num == 0 ) {
+                    sb.append("Par.");
+                } else if ( num == -1 ) {
+                    sb.append("Birdie.");
+                } else if ( num == -2 ) {
+                    sb.append("Eagle.");
+                } else if ( num <= -3 ) {
+                    sb.append("Double Eagle.");
+                }
+            }
+            i += 1;
+            sb.append("\n\n");
+        }
+        br.close();
+    }
+}
