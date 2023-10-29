aaa6543e5fc54fead2a59d4d14f94ce4c62b685d
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:46:23 +0900

    [Bronze IV] Title: Telemarketer or not?, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/16017. Telemarketer or not？/Telemarketer or not？.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+        String B = br.readLine();
+        String C = br.readLine();
+        String D = br.readLine();
+        if ( (A.equals("8") || A.equals("9")) 
+            && (D.equals("8") || D.equals("9")) 
+            && B.equals(C) ) {
+                System.out.print("ignore");
+        } else {
+            System.out.print("answer");
+        }
+        br.close();
+    }
+}
