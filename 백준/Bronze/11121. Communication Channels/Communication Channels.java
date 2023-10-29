6d1964ec7241c8d47c221764219aa1ecca5fedfb
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:37:45 +0900

    [Bronze IV] Title: Communication Channels, Time: 132 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/11121. Communication Channels/Communication Channels.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String result = "";
+        String len = br.readLine();
+        for ( int i = 0; i < Integer.parseInt(len); i++ ) {
+            String inputs = br.readLine();
+            if ( inputs.split(" ")[0].equals(inputs.split(" ")[1]) ) {
+                result += "OK\n";
+            } else {
+                result += "ERROR\n";
+            }
+        }
+        System.out.print(result);
+        br.close();
+    }
+}
