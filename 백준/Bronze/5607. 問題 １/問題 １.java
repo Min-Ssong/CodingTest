e2ac72713c2364a9ecc63596a29b0cdc90ed7327
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:25:07 +0900

    [Bronze III] Title: 問題 １, Time: 228 ms, Memory: 22616 KB -BaekjoonHub

---
+++ 백준/Bronze/5607. 問題 １/問題 １.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int numA = 0;
+        int numB = 0;
+        for ( int i = 0; i < N; i++ ) {
+            String AB = br.readLine();
+            int A = Integer.parseInt(AB.split(" ")[0]);
+            int B = Integer.parseInt(AB.split(" ")[1]);
+            if ( A == B ) {
+                numA += A;
+                numB += B;
+            } else if ( A > B ) {
+                numA += A + B;
+            } else if ( A < B ) {
+                numB += A + B;
+            }
+        }
+        System.out.print(numA + " " + numB);
+        br.close();
+    }
+}
