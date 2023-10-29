4e0393301647c186dad38a74c5a1d35b8d669671
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:49:54 +0900

    [Bronze IV] Title: 뉴비의 기준은 뭘까?, Time: 124 ms, Memory: 14376 KB -BaekjoonHub

---
+++ 백준/Bronze/19944. 뉴비의 기준은 뭘까？/뉴비의 기준은 뭘까？.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String allInput = br.readLine();
+        int N = Integer.parseInt(allInput.split(" ")[0]);
+        int M = Integer.parseInt(allInput.split(" ")[1]);
+        if ( M == 1 || M == 2 ) {
+            System.out.print("NEWBIE!");
+        } else if ( M <= N && !(M == 1 || M == 2)) {
+            System.out.print("OLDBIE!");
+        } else {
+            System.out.print("TLE!");
+        }
+        br.close();
+    }
+}
