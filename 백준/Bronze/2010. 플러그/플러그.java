8a4f3f3b3e0124ec051baffcb627b558f0f86143
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:03:48 +0900

    [Bronze III] Title: 플러그, Time: 288 ms, Memory: 40896 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+        for ( int i = 0; i < N; i++ ) {
+            int M = Integer.parseInt(br.readLine());
+            if ( i == N - 1 ) {
+                result += M;
+            } else {
+                result += M - 1;
+            }
+        }
+        System.out.print(result);
+        br.close();
+    }
+}
