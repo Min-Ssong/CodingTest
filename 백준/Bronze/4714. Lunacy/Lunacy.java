6219678b2fc6fbb58b7698fad112d42cd34292b7
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:20:06 +0900

    [Bronze IV] Title: Lunacy, Time: 252 ms, Memory: 18984 KB -BaekjoonHub

---
+++ 백준/Bronze/4714. Lunacy/Lunacy.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String result = "";
+        while ( true ) {
+            float inputs = sc.nextFloat();
+            if ( inputs == -1.0 ) {
+                break;
+            } else {
+                result += "Objects weighing " 
+                       + String.format("%.2f", inputs)
+                       + " on Earth will weigh "
+                       + String.format("%.2f", inputs * 0.167) 
+                       + " on the moon. \n";
+            }
+        }
+        System.out.print(result);
+
+        sc.close();
+    }
+}
