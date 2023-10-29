713899395c4cdc9ea6dc30a57bc54a92ae77f5dc
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:49:28 +0900

    [Bronze IV] Title: X に最も近い値 (The Nearest Value), Time: 136 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/18414. X に最も近い値 （The Nearest Value）/X に最も近い値 （The Nearest Value）.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String xlr = br.readLine();
+        int[] xlrInt = {Integer.parseInt(xlr.split(" ")[0]),
+            Integer.parseInt(xlr.split(" ")[1]),
+            Integer.parseInt(xlr.split(" ")[2])};
+        Arrays.sort(xlrInt);
+        System.out.print(xlrInt[1]);
+        br.close();
+    }
+}
