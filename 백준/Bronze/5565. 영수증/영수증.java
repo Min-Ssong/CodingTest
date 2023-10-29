b01a1d91f075929f43c1f251813fe9b4f36c1c15
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-10-29 21:24:18 +0900

    [Bronze III] Title: 영수증, Time: 124 ms, Memory: 13996 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int allPrice = Integer.parseInt(br.readLine());
+        int anotherPrice = 0;
+        for ( int i = 0; i < 9; i++ ) {
+            anotherPrice += Integer.parseInt(br.readLine());
+        }
+        System.out.println(allPrice - anotherPrice);
+        br.close();
+    }
+}
