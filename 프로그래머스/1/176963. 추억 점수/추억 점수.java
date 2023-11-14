3979aa383c355362efd898b17c8fdfe6720b26ea
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-11-14 20:55:15 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        HashMap<String, Integer> memory = new HashMap<String, Integer>();
+		for(int i = 0; i < name.length; i++){
+			memory.put(name[i], yearning[i]);
+		}
+		
+		int[] answer = new int[photo.length];
+		for(int i = 0; i < photo.length; i++){
+			for(int j = 0; j < photo[i].length; j++){
+				if(memory.containsKey(photo[i][j])){
+					answer[i] += memory.get(photo[i][j]);
+				}
+			}
+		}
+        return answer;
+    }
+}
