15eb201cd66ed1b4e5914b5308f6ca325d9f4338
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2023-11-14 20:54:56 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.62 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+class Solution {
+    public int solution(String s) {
+        HashMap<String, String> nMap = new HashMap<String, String>();
+        nMap.put("zero",  "0");
+		nMap.put("one",   "1");
+		nMap.put("two",   "2");
+		nMap.put("three", "3");
+		nMap.put("four",  "4");
+		nMap.put("five",  "5");
+		nMap.put("six",   "6");
+		nMap.put("seven", "7");
+		nMap.put("eight", "8");
+		nMap.put("nine",  "9");
+		
+		String temp = "";
+		String result = "";
+		for(char ch : s.toCharArray()){
+			if(Character.isDigit(ch)){
+				result += Character.toString(ch);
+			} else {
+				temp += ch;
+				if(nMap.containsKey(temp)){
+					result += nMap.get(temp);
+					temp = "";
+				}
+			}
+		}
+		return Integer.parseInt(result);
+    }
+}
