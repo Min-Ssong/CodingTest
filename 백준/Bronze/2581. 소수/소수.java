3a5edc1e7be810f7c75ca622ec0dbdf54777a2d1
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2025-03-17 17:32:19 +0900

    [Bronze II] Title: 소수, Time: 72 ms, Memory: 11488 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -6,34 +7,37 @@ public class Main {
         int M = Integer.parseInt(br.readLine());
         int N = Integer.parseInt(br.readLine());
 
+        boolean[] isPrime = new boolean[N + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        // 에라토스테네스의 체
+        for (int i = 2; i * i <= N; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
         int sum = 0;
         int min = Integer.MAX_VALUE;
 
-        for(int i = M; i <= N; i++){
-            if(isPrime(i)){
+        // M 이상 N 이하의 소수 찾기
+        for (int i = M; i <= N; i++) {
+            if (isPrime[i]) {
                 sum += i;
-                if(i < min){
+                if (i < min) {
                     min = i;
                 }
             }
         }
-        if(sum == 0){
+
+        if (sum == 0) {
             System.out.println(-1);
         } else {
             System.out.println(sum);
             System.out.println(min);
         }
-    }
-
-    public static boolean isPrime(int num){
-        if(num < 2){
-            return false;
-        }
-        for(int i = 2; i*i <= num; i++){
-            if(num % i == 0){
-                return false;
-            }
-        }
-        return true;
     }
 }
