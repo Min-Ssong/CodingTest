63abc4d4b0267a9faa5609939beffafa982912bc
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2025-03-17 16:30:10 +0900

    [Silver IV] Title: 듣보잡, Time: 220 ms, Memory: 25680 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int count = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -11,34 +10,22 @@ public class Main {
         int N = Integer.parseInt(numSt.nextToken());
         int M = Integer.parseInt(numSt.nextToken());
 
-        Set<String> set = new HashSet<>();
-        int count = 0;
-        List<String> overlap = new ArrayList<>();
-        for(int i = 0; i < N + M; i++){
+        Set<String> heard = new HashSet<>();
+        for(int i = 0; i < N; i++){
+            heard.add(br.readLine());
+        }
+
+        List<String> result = new ArrayList<>();
+        for(int i = 0; i < M; i++){
             String name = br.readLine();
-            if( set.contains(name) ){
-                overlap.add(name);
-                count++;
+            if( heard.contains(name) ){
+                result.add(name);
             }
-
-            set.add(name);
         }
 
-        Collections.sort(overlap);
-        
-        StringBuilder sb = new StringBuilder();
-        sb.append(count).append("\n");
-        for(int i = 0; i < overlap.size(); i++){
-            if(i == overlap.size() - 1) {
-                sb.append(overlap.get(i));
-                break;
-            }
-
-            sb.append(overlap.get(i)).append("\n");
-        }
-        bw.write(sb.toString());
+        Collections.sort(result);
+        bw.write(result.size() + "\n");
+        bw.write(String.join("\n", result));
         bw.flush();
-        bw.close();
-        br.close();
     }
 }
