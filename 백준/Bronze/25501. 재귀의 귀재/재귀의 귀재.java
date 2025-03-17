d1ae894d27636339979cb8d0775d4f2457c47048
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2025-03-17 11:34:16 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 144 ms, Memory: 20048 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -1,37 +1,30 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static int count = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < N; i++) {
-            sb.append(isPalindrome(br.readLine().toCharArray()) + "\n");
+            char[] ch = br.readLine().toCharArray();
+            int[] count = {0}; // 호출 횟수를 배열로 관리
+            int result = recursion(ch, 0, ch.length - 1, count);
+            sb.append(result).append(" ").append(count[0]).append("\n");
         }
+
         bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
 
-    public static String isPalindrome(char[] ch){
-        count = 0;
-        return String.valueOf(recursion(ch, 0, ch.length - 1)) +" "+ count;
-    }
-
-    public static int recursion(char[] ch, int l, int r){
-        count++;
-
-        if( l >= r ) {
-            return 1;
-        } else if( ch[l] != ch[r] ){
-            return 0;
-        } else {
-            return recursion(ch, l+1, r-1);
-        }
+    public static int recursion(char[] ch, int l, int r, int[] count) {
+        count[0]++; // 호출 횟수 증가
+        if (l >= r) return 1;
+        else if (ch[l] != ch[r]) return 0;
+        else return recursion(ch, l + 1, r - 1, count);
     }
 }
