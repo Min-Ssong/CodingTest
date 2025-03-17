1f76601d554f8cffbe438545cc3e709911597ad0
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2025-03-17 11:14:21 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 68 ms, Memory: 11592 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,19 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		System.out.println(fibonacci(N));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        bw.write(String.valueOf(fibo(num)));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    public static int fibonacci(int n){
-		if(n <= 1){
-			return n;
-		} else {
-			return fibonacci(n-1) + fibonacci(n-2);
-		}
-	}
+    public static int fibo(int n){
+        if( n == 0 ) return 0;
+        if( n == 1 || n == 2 ) return 1;
+        return fibo(n - 2) + fibo(n - 1);
+    }
 }
