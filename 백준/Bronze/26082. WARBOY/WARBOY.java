7823cb3beb0638781fcc4e77d4e43ce94c04fff4
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2025-03-20 06:45:29 +0900

    [Bronze V] Title: WARBOY, Time: 64 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int result = B / A * 3 * C;
+        bw.write(String.valueOf(result));
+        bw.flush();
+    }
+}
