570fa28d8d11a44265bd5044a35537f3dab9ba3d
MinSeong Jeon <49707925+Min-Ssong@users.noreply.github.com>
2025-03-27 20:08:09 +0900

    [Bronze III] Title: A+B - 6, Time: 64 ms, Memory: 11664 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+            sb.append(num1 + num2).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
